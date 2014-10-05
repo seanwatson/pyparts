875877bbbe4d2cec5138fa0755a87cc207d0cc66
seanwatson <seanwatson@server.fake>
2014-10-05 00:15:18 -0700

    creating empty readme and license. added rgb example

---
+++ examples/rgb_led.py
@@ -0,0 +1,49 @@
+import time
+
+from pyparts.component.components import Components
+from pyparts.platfom import raspberrypi_platform as rpi_platform
+
+RED_PIN = 1
+GREEN_PIN = 2
+BLUE_PIN = 3
+
+PIN_NAMING_SCHEME = rpi_platform.BCM
+PWM_FREQUENCY_HZ = 2000
+
+
+def main():
+  platform = rpi_platform.RasberryPiPlatform(PIN_NAMING_SCHEME)
+  components = Components(platform)
+
+  rgb = components.get_rgb_led(RED_PIN, GREEN_PIN, BLUE_PIN)
+
+  rgb.set_frequency(PWM_FREQUENCY_HZ)
+  print 'PWM Frequency:', rgb.get_frequency
+
+  rgb.set_rgb(25, 50, 75)
+  print '(Red, Green, Blue):', rgb.get_rgb()
+
+  rgb.enable()
+  print 'Led enabled:' rgb.is_enabled()
+
+  time.sleep(5)
+
+  rgb.set_red(40)
+  rgb.set_green(20)
+  rgb.set_blue(0)
+  print 'Red:', rgb.get_red()
+  print 'Greed:', rgb.get_green()
+  print 'Blue:', rgb.get_blue()
+
+  time.sleep(5)
+
+  rgb.fade(0, 0, 100)
+
+  time.sleep(5)
+
+  rgb.disable()
+  print 'Led enabled:' rgb.is_enabled()
+
+
+if __name__ == '__main__':
+  self.main()
