18cdcf3efa38cef6ece176a727f7e88e9f19f095
seanwatson <seanwatson@server.fake>
2014-11-28 20:31:10 -0800

    adding pid controller and temp controller)

---
+++ src/logic/pid_controller.py
@@ -0,0 +1,58 @@
+import time
+import threading
+
+
+class PIDController(object):
+
+  def __init__(self, kp, ki, kd):
+    self._kp = kp
+    self._ki = ki
+    self._kd = kd
+
+    self._prev_error = 0
+    self._cp = 0
+    self._ci = 0
+    self._cd = 0
+
+    self._prev_time = time.time()
+
+  def get_output(self, error):
+    self._current_time = time.time()
+    dt = self._current_time - self._prev_time
+    de = error - self._prev_error
+
+    self._cp = self._kp * error
+    self._ci += error * dt
+    self._cd = 0
+    if dt > 0:
+      self._cd = de / dt
+
+    self._prev_time = self._current_time
+    self._prev_error = error
+
+    return self._cp  + (self._ki * self._ci) + (self._kd * self._cd)
+
+  class Worker(threading.Thread):
+
+    def __init__(self, kp, kd, ki, input_func, output_func):
+      super(Worker, self).__init__()
+      self._controller = PIDController(kp, ki, kd)
+      self._input_func = input_func
+      self._output_func = output_func
+      self._set_point = 0
+      self._stop = False
+
+    def set_desired_value(self, value):
+      self._set_point = value
+
+    def get_desired_value(self):
+      return self._set_point
+
+    def stop(self):
+      self._stop = True
+
+    def run(self):
+      while not self._stop:
+        current_val = self._input_func()
+        error = self._set_point - current_val
+        self._output_func(self._controller.get_output())
---
+++ src/systems/temperature_controller.py
@@ -0,0 +1,39 @@
+import time
+
+from logic import pid_controller
+
+class TemperatureController(object):
+
+  MAX_ERROR_DEGREES_C = 10.0
+
+  def __init__(self, temp_sensor, heater_pin, kp, ki, kd):
+    self._temp_sensor = temp_sensor
+    self._heater_pin = heater_pin
+    self._pid_worker = pid_controller.PIDController.Worker(
+        kp, ki, kd, self._pid_input_func, self._pid_output_func)
+
+  def _pid_input_func(self):
+    return self._time_sensor.get_temp_c()
+
+  def _pid_output_func(self, val):
+    # If the sensor is too hot, turn off the heater
+    if val <= 0:
+      self._heater_pin.set_duty_cycle(0)
+      return
+    if val > MAX_ERROR_DEGREES_C:
+      self._heater_pin.set_duty_cycle(100)
+      return
+    self._heater_pin.set_duty_cycle(float(val) / self.MAX_ERROR_DEGREES_C * 100)
+    time.sleep(0.1)
+
+  def set_temp_c(self, temp):
+    self._pid_worker.set_desired_value(temp)
+
+  def get_temp_setting(self):
+    return self._pid_worker.get_desired_value()
+
+  def enable(self):
+    self._pid_worker.start()
+
+  def disable(self):
+    self._pid_worker.stop()
